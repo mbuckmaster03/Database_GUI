d43bd06b26d52d746d55e164d66e84c383df28a8
mbuckmaster03 <mbuckmaster@stcharlessd.org>
2020-02-11 11:17:42 -0600

    modified:   database_gui.py 	new file:   untitled-1.py

--- untitled-1.py
+++ untitled-1.py
@@ -14,13 +14,7 @@ TITLE_FONT = ("Times New Roman", 24)
 
 BOTTOM_FONT = ("Arial", 15)
 
-class MainMenu(tk.Frame):
-    def __init__(self):
-        tk.Frame.__init__(self)
-        self.lbl_title = tk.Label(text = "Game Library", font = TITLE_FONT)
-        self.lbl_title.grid(row = 0, column = 0, sticky = "news")
-        self.btn_add = tk.Button(text = "Add", font = BUTTON_FONT)
-        self.btn_add.grid(row = 1, column = 0)
+
 
 
 
@@ -33,9 +27,6 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("Game Lib")
     root.geometry("500x500")
-    main_menu = MainMenu()
-    main_menu.grid(row = 0, column = 0)
-    
     
     
     root.mainloop()
